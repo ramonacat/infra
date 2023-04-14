67f87de1655be2ada5d9ae2e8ce5b30037db1862
Ramona Łuczkiewicz <ja@agares.info>
2023-04-14 20:37:02 +0200

    Create an initial set of k8s nodes and a jump host

--- hcloud.tf
+++ hcloud.tf
@@ -28,10 +28,64 @@ resource "hcloud_ssh_key" "ramona" {
     public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCatH7XWmY6oZPSe3woP2swvJ4/stZrpaVWNg6FMcs87xEtCr/sIkj/rm41gD6F3k3Z6jhxqBKgZcr45aW07xlB//KfYs3kb0PYDsn3KrwCPBjHwRypuPvyagCUDAbD9wnhpEr9iHEbhW2yNEDC5E1c3ak/fNjewCZMpqo645gQ6siFAnwEnqTQR0lF3B/hdmAA/j+efQ3ghjiI6+O3uQ0o5coCNa4tCrq3yqsyA7eI0jhT1Ij8SE54ren3dwndq1JoGNg7DCtozl3fCgHVUrdWeW2kcB1A/Ta+jcmcB10Rv9ZevU2wYvZIEYXG1hSjM8Zrr7JwAcXkG/mb3lGnYnU49YxNqT4vwD0ZyY8d5M9Hvw065+y7Y45+/ScevmIGn/fn/9TbZHdPdSKM1UFMICUctT6VH6ShhEkbiQ38E3GnA1n3mnsOnxaBT5hVJxr13yLV8ULU/8not6SMU/3xP2rZj6JP7xtHJP/29Nd4N7gm6adz3wbS1aRJosVr3ZbA1qTaB/m4EBRTfNYtifUbdQkFbrnlNmVNb5ixhS1ZLZq4aRPmp6MH034sQ9HZSrtMMSO5B9TXHCb3zxexR6BBtIjZHBqwuu3krMWh9kOW3wNFWmEWdy5vLUcVVoXSaGqICQwG/HOKGNdzGumFDnPfvayVVCxu67s2b82oTtkbd+mjMQ== openpgp:0xCF7158EB"
 }
 
-resource "hcloud_server" "node1" {
-    name = "node1"
+resource "hcloud_network" "mainnet" {
+    name = "mainnet"
+    ip_range = "10.69.100.0/24"
+}
+
+resource "hcloud_network_subnet" "mainnet-subnet0" {
+    network_id = hcloud_network.mainnet.id
+    type = "cloud"
+    ip_range = "10.69.100.0/24"
+    network_zone = "eu-central"
+}
+
+resource "hcloud_server" "jump" {
+    name = "jump"
+    location = "nbg1"
     image = "ubuntu-22.04"
     server_type = "cax11"
     ssh_keys = [ hcloud_ssh_key.ramona.id ]
     user_data = "#cloud-config\nruncmd:\n- curl https://raw.githubusercontent.com/elitak/nixos-infect/master/nixos-infect | PROVIDER=hetznercloud NIX_CHANNEL=nixos-unstable bash 2>&1 | tee /tmp/infect.log\n"
+
+    network {
+        network_id = hcloud_network.mainnet.id
+        ip = "10.69.100.1"
+    }
+
+    depends_on = [
+      hcloud_network_subnet.mainnet-subnet0
+    ]
+}
+
+locals {
+    k8s-nodes = {
+        node0 = { ip = "10.69.100.2" },
+        node1 = { ip = "10.69.100.3" },
+        node2 = { ip = "10.69.100.4" },
+    }
+}
+
+resource "hcloud_server" "nodes" {
+    for_each = local.k8s-nodes
+    name = each.key
+    location = "nbg1"
+    image = "ubuntu-22.04"
+    server_type = "cax11"
+    ssh_keys = [ hcloud_ssh_key.ramona.id ]
+    user_data = "#cloud-config\nruncmd:\n- curl https://raw.githubusercontent.com/elitak/nixos-infect/master/nixos-infect | PROVIDER=hetznercloud NIX_CHANNEL=nixos-unstable bash 2>&1 | tee /tmp/infect.log\n"
+
+    public_net {
+      ipv4_enabled = false
+      ipv6_enabled = false
+    }
+
+    network {
+        network_id = hcloud_network.mainnet.id
+        ip = each.value.ip
+    }
+
+    depends_on = [
+      hcloud_network_subnet.mainnet-subnet0
+    ]
 }
