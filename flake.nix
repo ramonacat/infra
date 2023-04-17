fd0d7c6874f8b926da8908e091c202dd0b74ea09
Ramona Łuczkiewicz <ja@agares.info>
2023-04-17 14:52:58 +0200

    Initial setup for remote building of the jump host

--- flake.nix
+++ flake.nix
@@ -3,14 +3,24 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
-    in {
-      devShells.default = pkgs.mkShell {
-        packages = [ 
-          pkgs.terraform
+  outputs = { self, nixpkgs, flake-utils }: {
+    formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixpkgs-fmt;
+    devShells.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.mkShell {
+      shellHook = ''
+        rustup default stable
+      '';
+      packages = with nixpkgs.legacyPackages.x86_64-linux; [
+        terraform
+        rustup
+      ];
+    };
+    nixosConfigurations = {
+      jump = nixpkgs.lib.nixosSystem {
+        system = "aarch64-linux";
+        modules = [
+          ./machines/jump.nix
         ];
       };
-    });
+    };
+  };
 }
