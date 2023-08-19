fb81d4d090b6b26d43e6b676d97c9fbbe2634a4b
Ramona Łuczkiewicz <ja@agares.info>
2023-08-19 14:52:06 +0200

    Connect to the database

--- apps/backend/src/main.rs
+++ apps/backend/src/main.rs
@@ -2,9 +2,25 @@ use axum::{
     routing::get,
     Router,
 };
+use sqlx::postgres::PgPoolOptions;
+
+mod secrets;
 
 #[tokio::main]
 async fn main() {
+    let username = secrets::read("db-backend-app", "username").expect("Failed to read username");
+    let password = secrets::read("db-backend-app", "password").expect("Failed to read password");
+
+    let db_pool = PgPoolOptions::new()
+        .max_connections(50) // TODO: tune
+        .connect(&format!("postgres://{}:{}@db-backend/db-backend", username, password))
+        .await
+        .expect("Database connection failed");
+
+    let query_result: (String,) = sqlx::query_as("SELECT NOW()").fetch_one(&db_pool).await.expect("Database query failed");
+
+    println!("Database time: {}", query_result.0);
+
     // build our application with a single route
     let app = Router::new()
         // TODO is it possible to set the base path?
---
+++ apps/backend/src/secrets.rs
@@ -0,0 +1,7 @@
+use std::path::{Path};
+
+pub fn read<'a, 'b>(name: impl Into<&'a str>, file: impl Into<&'b str>) -> Result<String, std::io::Error> {
+    let path = Path::new("/etc/secrets/").join(name.into()).join(file.into());
+    
+    std::fs::read_to_string(path)
+}
