95594cc4415ce34ccd2008eb9ab033271ef80ec7
Ramona Łuczkiewicz <ja@agares.info>
2023-08-19 15:17:47 +0200

    Define the database

--- apps/backend/src/secrets.rs
+++ apps/backend/src/secrets.rs
@@ -1,4 +1,4 @@
-use std::path::{Path};
+use std::path::Path;
 
 pub fn read<'a, 'b>(name: impl Into<&'a str>, file: impl Into<&'b str>) -> Result<String, std::io::Error> {
     let path = Path::new("/etc/secrets/").join(name.into()).join(file.into());
