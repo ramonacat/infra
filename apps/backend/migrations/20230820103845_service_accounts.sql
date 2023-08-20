ddec7b33f18395e07c4f744b8f94f857aa9b8219
Ramona Łuczkiewicz <ja@agares.info>
2023-08-20 12:51:13 +0200

    Missing semicolons

--- apps/backend/migrations/20230820103845_service_accounts.sql
+++ apps/backend/migrations/20230820103845_service_accounts.sql
@@ -1,10 +1,10 @@
 CREATE TABLE service_accounts (
     id UUID PRIMARY KEY,
     name TEXT NOT NULL
-)
+);
 
 CREATE TABLE service_account_tokens {
     id UUID PRIMARY KEY,
     service_account UUID REFERENCES(service_accounts.id) ON DELETE CASCADE,
     content TEXT NOT NULL
-}
+};
