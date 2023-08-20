a4191f01e0ff7a942b322c50461968c778b27906
Ramona Łuczkiewicz <ja@agares.info>
2023-08-20 12:59:24 +0200

    Syntax errors in migrations

--- apps/backend/migrations/20230820103845_service_accounts.sql
+++ apps/backend/migrations/20230820103845_service_accounts.sql
@@ -3,8 +3,8 @@ CREATE TABLE service_accounts (
     name TEXT NOT NULL
 );
 
-CREATE TABLE service_account_tokens {
+CREATE TABLE service_account_tokens (
     id UUID PRIMARY KEY,
     service_account UUID REFERENCES(service_accounts.id) ON DELETE CASCADE,
     content TEXT NOT NULL
-};
+);
