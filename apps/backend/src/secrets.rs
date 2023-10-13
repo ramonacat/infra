dfe70db725875bb2c6d744323c7ad374374713fa
Ramona Łuczkiewicz <ja@agares.info>
2023-10-13 15:47:25 +0200

    Add a local development configuration

--- apps/backend/src/secrets.rs
+++ apps/backend/src/secrets.rs
@@ -1,12 +1,39 @@
 use std::path::Path;
 
-pub fn read<'a, 'b>(
-    name: impl Into<&'a str>,
-    file: impl Into<&'b str>,
-) -> Result<String, std::io::Error> {
-    let path = Path::new("/etc/secrets/")
-        .join(name.into())
-        .join(file.into());
+use thiserror::Error;
+use tracing::debug;
 
-    std::fs::read_to_string(path)
+#[derive(Debug, Error)]
+pub enum Error {
+    #[error("Failed to read secret ({1}, {2}): {0}")]
+    Io(std::io::Error, String, String),
+}
+
+pub fn read(name: impl Into<String>, file: impl Into<String>) -> Result<String, Error> {
+    let name: String = name.into();
+    let file: String = file.into();
+
+    #[cfg(debug_assertions)]
+    {
+        let variable_name = format!(
+            "SECRET_{}_{}",
+            name.replace('-', "_"),
+            file.replace('-', "_")
+        );
+        debug!(
+            "Debug build, reading secret from env variable: {}",
+            &variable_name
+        );
+        let env = std::env::var(variable_name);
+
+        if let Ok(value) = env {
+            return Ok(value);
+        }
+    }
+
+    let path = Path::new("/etc/secrets/")
+        .join(name.clone())
+        .join(file.clone());
+
+    std::fs::read_to_string(path).map_err(|e| Error::Io(e, name, file))
 }
